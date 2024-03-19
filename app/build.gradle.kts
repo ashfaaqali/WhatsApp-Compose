49cd3b30d21ffb5f7206f9fca0653cb62c773d2b
Mohd Ashfaq <tbeast305@gmail.com>
2024-03-19 10:03:56 +0530

    navigation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,6 +57,9 @@ dependencies {
     // GSON
     implementation("com.google.code.gson:gson:2.10.1")
 
+    // Navigation
+    implementation ("androidx.navigation:navigation-compose:2.7.7")
+
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
     implementation ("androidx.compose.runtime:runtime-livedata:1.6.3")
