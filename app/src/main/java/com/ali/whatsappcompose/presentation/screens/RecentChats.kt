314aef420de78c41ef078fe3c9588454adf35e58
Mohd Ashfaq <tbeast305@gmail.com>
2024-03-19 10:08:13 +0530

    Implement item click in recent chats

--- app/src/main/java/com/ali/whatsappcompose/presentation/screens/RecentChats.kt
+++ app/src/main/java/com/ali/whatsappcompose/presentation/screens/RecentChats.kt
@@ -1,24 +1,26 @@
 package com.ali.whatsappcompose.presentation.screens
 
+import android.util.Log
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.ali.whatsappcompose.presentation.navigation.Route
 import com.ali.whatsappcompose.presentation.viewmodel.RecentChatsViewModel
 
 @Composable
-fun RecentChats(viewModel: RecentChatsViewModel) {
+fun RecentChats(onClick: () -> Unit) {
+    val viewModel: RecentChatsViewModel = viewModel()
     val recentChats by viewModel.recentChats.observeAsState(initial = emptyList())
+    Log.d("RecentChats", recentChats.toString())
 
     if (recentChats.isNotEmpty()) {
         LazyColumn(content = {
             items(recentChats) { chat ->
-                RecentChatsItem(recentChat = chat)
+                RecentChatsItem(recentChat = chat, onClick)
             }
         })
-    } else {
-        Text(text = "Loading...")
     }
 }
